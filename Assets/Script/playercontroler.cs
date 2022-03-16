1bac25044dbf8303408903fa4601be4d339feb8e
Paulastya2007 <86978708+Paulastya2007@users.noreply.github.com>
2022-03-16 14:32:35 +0530

    Partially Complete

--- Assets/Script/playercontroler.cs
+++ Assets/Script/playercontroler.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 public class playercontroler : MonoBehaviour
-{public Rigidbody rb;private float x, z;
-        void Start()
+{public Rigidbody rb;private float x, z;public int a;public Text b;public GameObject gp;
+    private void OnTriggerEnter(Collider other)
+    {if (other.gameObject.CompareTag("pickup"))
+      {other.gameObject.SetActive(false);a++;
+            b.text = a.ToString(); if (a >= 8) { 
+            gp.SetActive(true);
+            }
+        }
+    }
+    void Start()
     {
+        gp.SetActive(false);
+        a= 0;
         rb=GetComponent<Rigidbody>();
     }
     void OnMove(InputValue tk)
