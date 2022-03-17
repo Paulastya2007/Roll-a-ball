095136c11b6222ccf243e765ed206f1f583993bd
Paulastya2007 <86978708+Paulastya2007@users.noreply.github.com>
2022-03-17 13:16:32 +0530

    project complete

---
+++ Assets/Script/done.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class done : MonoBehaviour
+{public void sdfg() {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Script/men.cs
@@ -0,0 +1,28 @@
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class men : MonoBehaviour
+{
+    public GameObject nmap,old;public string mp = "https://github.com/Paulastya2007/Tut";
+    void Start()
+    {
+        old.SetActive(true);
+        nmap.SetActive(false);
+    }
+    public void play() {
+        SceneManager.LoadScene(1);
+    }
+    public void quit()
+    { 
+        Application.Quit();
+    }
+    public void ClickHere() {
+        Application.OpenURL(mp);
+    }
+    public void crap() {
+        nmap.SetActive(true);old.SetActive(false);
+    }
+    public void back() { old.SetActive(true); nmap.SetActive(false); }
+}
