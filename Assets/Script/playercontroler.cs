0c3a6c95d093fddb85994e795ca487ed14f1a54c
Paulastya2007 <rajachakraborty2007@gmail.com>
2022-04-03 10:52:51 +0530

    Some improvements

--- Assets/Script/playercontroler.cs
+++ Assets/Script/playercontroler.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
 public class playercontroler : MonoBehaviour
-{public Rigidbody rb;private float x, z;public int a;public Text b;public GameObject gp;
-    private void OnTriggerEnter(Collider other)
+{public Rigidbody rb;private float x, z;public int  pmk = 0, a;public Text timer,b;public GameObject gp;
+        private void OnTriggerEnter(Collider other)
     {if (other.gameObject.CompareTag("pickup"))
       {other.gameObject.SetActive(false);a++;
             b.text = a.ToString(); if (a >= 8) { 
             gp.SetActive(true);
+Time.timeScale = 0;
             }
         }
     }
@@ -24,9 +25,11 @@ public class playercontroler : MonoBehaviour
         Vector2 ipm = tk.Get<Vector2>();
         x = ipm.x;z=ipm.y;
     }
+
     void FixedUpdate()
     {//transform.translate(0,0,0); this moves witout rigidbody without gravity
         Vector3 mt= new Vector3(x,0,z);
-        rb.AddForce(mt*11);
-    }
+        rb.AddForce(mt*11); pmk++;
+        timer.text = pmk.ToString();
+            }
 }
